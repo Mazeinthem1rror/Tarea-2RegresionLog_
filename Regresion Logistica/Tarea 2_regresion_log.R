1e5e0d880e96c75dd492517ffa05b62c37009678
Josefa Espinoza <espinozajosefam@gmail.com>
2022-06-20 19:20:05 -0400

    agregué paquetes y las variables a trabajar

--- Regresion Logistica/Tarea 2_regresion_log.R
+++ Regresion Logistica/Tarea 2_regresion_log.R
@@ -1,24 +1,25 @@
 ##Tarea 2 Regresion logistica
+#Estudiantes: Josefa Espinoza, Mistian Herrera, Maximiliano Levenier y Macarena Pol.
 #Carga paquetes
 install.packages("pacman")
-pacman::p_load(haven, sjPolt, car, dplyr,magrittr, sjmisc)
+pacman::p_load(haven, sjPolt, car, dplyr,magrittr, sjmisc, 
+               ResourceSelection,DescTools, performance)
+
+options(scipen = 999) 
 
 ##Cargar datos
 base_enusc_2020_sav <- read_sav("input/data/base-enusc-2020-sav.sav")
 
-#Explorar datos
-modelo1<-Enusc_2020 %>%
-    select(P1_1_1, enc_region)
-  #Recodificacion
 
-frq(modelo1$P1_1_1)
-frq(modelo1$enc_region)
+#Variables a trabajar
+#P3-> ¿Cree ud que será victima de un delito en los proximos 12 meses?
+#R3-> Edad
+#R4-> Sexo
+#enc_region-> Región
 
-modelo1<- modelo1 %>% mutate(P1_recode = case_when(P1_1_1==1 ~ "Aumentó",
-                                                      P1_1_1==3~ "Disminuyó",
-                                                      TRUE~NA_character_), 
-                             reg_recode =if_else(enc_region ==13, 1,0))
-frq(modelo1$P1_recode)
-frq(modelo1$reg_recode)
-#holaa
-#olaa
+
+#Recodificacion
+
+reg_recode =if_else(enc_region ==13, 1,0)
+frq(reg_recode)
+
