3b142362a6786d46fa171f9648019998d7085ec8
Josefa Espinoza <espinozajosefam@gmail.com>
2022-06-21 21:53:48 -0400

    variables
    .

--- Regresion Logistica/Tarea 2_regresion_log.R
+++ Regresion Logistica/Tarea 2_regresion_log.R
@@ -7,18 +7,20 @@ pacman::p_load(haven, sjPolt, car, dplyr,magrittr, sjmisc,
 
 options(scipen = 999) 
 
-##Cargar datos
+##Cargar datos----------------------------------------------------------------
 base_enusc_2020_sav <- read_sav("input/data/base-enusc-2020-sav.sav")
 
 
-#Variables a trabajar
+#Variables a trabajar----------------------------------------------------------
 #P3-> ¿Cree ud que será victima de un delito en los proximos 12 meses?
 #R3-> Edad
 #R4-> Sexo
 #enc_region-> Región
+#A1-> Durante los últimos doce meses, ¿usted o algún miembro de su hogar fue asaltado usando violencia, amenaza o intimidación? ¿Cuántas veces?
+#P1_3_1-> (Barrio)pensando en la delincuencia,¿ud diria que la delincuencia en su barrio aumentó disminuyó o se mantuvo?
+#P1_1_1-> (Pais) pensando en la delincuencia,¿ud diria que la delincuencia en su barrio aumentó disminuyó o se mantuvo?
 
-
-#Recodificacion
+#Recodificacion----------------------------------------------------------------
 
 reg_recode =if_else(enc_region ==13, 1,0)
 frq(reg_recode)
